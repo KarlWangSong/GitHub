4a22d45bc71e2e7a93df13ecd8e43c13af8c20fd
KarlWangSong <773678369@qq.com>
2016-07-08 21:11:05 +0800

    update 02
    update 02

--- src/com/atguigu/github/test/HelloWorld.java
+++ src/com/atguigu/github/test/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub!");
 		System.out.println("update 01");
+		System.out.println("update 02 by service");
 	}
 
 }
